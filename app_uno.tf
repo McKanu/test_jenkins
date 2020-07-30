90f71c7c5780f73c60d96be152d23b2c126daeaf
Aki Isokangas <aki.isokangas@teliacompany.com>
2020-07-30 16:29:36 +0300

    App Uno added

---
+++ app_uno.tf
@@ -0,0 +1,18 @@
+resource "aci_application_profile" "APP_NUMERO_DOS" {
+  tenant_dn = aci_tenant.INT_JAB.id
+  name      = "APP_NUMERO_DOS"
+}
+
+resource "aci_application_epg" "EPG_FE" {
+  application_profile_dn = aci_application_profile.APP_NUMERO_DOS.id
+  name                   = "EPG_FE"
+  relation_fv_rs_bd      = aci_bridge_domain.INT_JAB_BD.id
+  relation_fv_rs_cons    = [aci_contract.baseline.id]
+}
+
+resource "aci_application_epg" "EPG_BE" {
+  application_profile_dn = aci_application_profile.APP_NUMERO_DOS.id
+  name                   = "EPG_BE"
+  relation_fv_rs_bd      = aci_bridge_domain.INT_JAB_BD.id
+  relation_fv_rs_prov    = [aci_contract.baseline.id]
+}
