0d626c6ee732f7f1bd3f90b49b99d498e6859eb3
Aki Isokangas <aki.isokangas@teliacompany.com>
2020-07-30 16:38:32 +0300

    New versions

--- app_dos.tf
+++ app_dos.tf
@@ -1,17 +1,17 @@
-resource "aci_application_profile" "APP_NUMERO_UNO" {
+resource "aci_application_profile" "APP_NUMERO_DOS" {
   tenant_dn = aci_tenant.INT_JAB.id
-  name      = "APP_NUMERO_UNO"
+  name      = "APP_NUMERO_DOS"
 }
 
 resource "aci_application_epg" "EPG_FE" {
-  application_profile_dn = aci_application_profile.APP_NUMERO_UNO.id
+  application_profile_dn = aci_application_profile.APP_NUMERO_DOS.id
   name                   = "EPG_FE"
   relation_fv_rs_bd      = aci_bridge_domain.INT_JAB_BD.id
   relation_fv_rs_cons    = [aci_contract.baseline.id]
 }
 
 resource "aci_application_epg" "EPG_BE" {
-  application_profile_dn = aci_application_profile.APP_NUMERO_UNO.id
+  application_profile_dn = aci_application_profile.APP_NUMERO_DOS.id
   name                   = "EPG_BE"
   relation_fv_rs_bd      = aci_bridge_domain.INT_JAB_BD.id
   relation_fv_rs_prov    = [aci_contract.baseline.id]
