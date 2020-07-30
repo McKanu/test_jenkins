8605f09e230a4f4b6ed2ca4c77f83c98e999781e
Aki Isokangas <aki.isokangas@teliacompany.com>
2020-07-30 16:40:15 +0300

    Modified aci_application_epg resource-names

--- app_dos.tf
+++ app_dos.tf
@@ -3,14 +3,14 @@ resource "aci_application_profile" "APP_NUMERO_DOS" {
   name      = "APP_NUMERO_DOS"
 }
 
-resource "aci_application_epg" "EPG_FE" {
+resource "aci_application_epg" "EPG_FE_DOS" {
   application_profile_dn = aci_application_profile.APP_NUMERO_DOS.id
   name                   = "EPG_FE"
   relation_fv_rs_bd      = aci_bridge_domain.INT_JAB_BD.id
   relation_fv_rs_cons    = [aci_contract.baseline.id]
 }
 
-resource "aci_application_epg" "EPG_BE" {
+resource "aci_application_epg" "EPG_BE_DOS" {
   application_profile_dn = aci_application_profile.APP_NUMERO_DOS.id
   name                   = "EPG_BE"
   relation_fv_rs_bd      = aci_bridge_domain.INT_JAB_BD.id
--- app_uno.tf
+++ app_uno.tf
@@ -3,14 +3,14 @@ resource "aci_application_profile" "APP_NUMERO_UNO" {
   name      = "APP_NUMERO_UNO"
 }
 
-resource "aci_application_epg" "EPG_FE" {
+resource "aci_application_epg" "EPG_FE_UNO" {
   application_profile_dn = aci_application_profile.APP_NUMERO_UNO.id
   name                   = "EPG_FE"
   relation_fv_rs_bd      = aci_bridge_domain.INT_JAB_BD.id
   relation_fv_rs_cons    = [aci_contract.baseline.id]
 }
 
-resource "aci_application_epg" "EPG_BE" {
+resource "aci_application_epg" "EPG_BE_UNO" {
   application_profile_dn = aci_application_profile.APP_NUMERO_UNO.id
   name                   = "EPG_BE"
   relation_fv_rs_bd      = aci_bridge_domain.INT_JAB_BD.id
