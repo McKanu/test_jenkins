47c645f6f5e28796e32b99599de46f4583beddf4
Aki Isokangas <aki.isokangas@teliacompany.com>
2020-07-31 14:04:37 +0300

    AP HID100004123 NMS-net

---
+++ HID100004123.tf
@@ -0,0 +1,18 @@
+resource "aci_application_profile" "AP_HID100004123" {
+  tenant_dn = aci_tenant.INT_JAB.id
+  name      = "AP_HID100004123"
+}
+
+resource "aci_application_epg" "EPG_HID100004123_FE" {
+  application_profile_dn = aci_application_profile.AP_HID100004123.id
+  name                   = "EPG_HID100004123_FE"
+  relation_fv_rs_bd      = aci_bridge_domain.INT_JAB_BD.id
+  relation_fv_rs_cons    = [aci_contract.baseline.id]
+}
+
+resource "aci_application_epg" "EPG_HID100004123_BE" {
+  application_profile_dn = aci_application_profile.AP_HID100004123.id
+  name                   = "EPG_HID100004123_BE"
+  relation_fv_rs_bd      = aci_bridge_domain.INT_JAB_BD.id
+  relation_fv_rs_prov    = [aci_contract.baseline.id]
+}
